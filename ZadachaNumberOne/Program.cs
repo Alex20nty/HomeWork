0631f146d8af1daedd6376e4da00d1d9b7def021
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-17 03:12:08 +1100

    Updated ZadachaNumberOne

--- ZadachaNumberOne/Program.cs
+++ ZadachaNumberOne/Program.cs
@@ -1,12 +1,14 @@
-﻿int a = -9;
-int b = -3;
+﻿int a = 2;
+int b = 10;
 
 if(a > b)
 {
     Console.WriteLine(a);
+    Console.WriteLine(b);
 }
 
 else
 {
     Console.WriteLine(b);
+    Console.WriteLine(a);
 }
