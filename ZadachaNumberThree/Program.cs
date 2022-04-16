d8a7944d0f0be37740f02168127893d1de4dec02
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-17 04:10:13 +1100

    Add ZadachaNumberThree

---
+++ ZadachaNumberThree/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = 9;
+int C;
+
+C = a%2;
+
+if (C==0)
+{
+Console.Write("C - четное число");
+Console.WriteLine(C);
+}
+else
+{
+    Console.Write("C - нечетное число");
+    Console.WriteLine(C);
+}
+
