1f0439b88699954d08f1185c9d1e655d0c28c724
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-17 03:48:02 +1100

    Add ZadachaNumberTwo

---
+++ ZadachaNumberTwo/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 22;
+int b = 897;
+int c = 35;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+
