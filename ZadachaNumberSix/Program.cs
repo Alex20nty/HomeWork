cc89310bc93d5a976ac83ee3982a2ea85352ed29
Алексей Телепнев <alekseytelepnev@MacBook-Pro-Aleksej.local>
2022-04-20 02:02:27 +1100

    Add ZadachaNumberSix

---
+++ ZadachaNumberSix/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >=100 && n<=999)
+{
+    int i = n%10;
+    Console.Write(i);
+}
+else
+{
+    Console.WriteLine("Введи ТРЕХзначное число! ");
+}
